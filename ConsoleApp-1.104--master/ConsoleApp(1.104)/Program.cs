0c7aebbc4e85a95efde8ade71ed279d0c9e2b36a
Gurin18 <114131242+Gurin18@users.noreply.github.com>
2023-01-29 09:26:07 +0300

    Add files via upload

---
+++ ConsoleApp-1.104--master/ConsoleApp(1.104)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication7
+{
+    public enum DayOfWeek
+    {
+        Понедельник = 0,
+        Вторник = 1,
+        Среда = 2,
+        Четверг = 3,
+        Пятницa = 4,
+        Суббота = 5,
+        Воскресенье = 6
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("n=");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine((DayOfWeek)((n - 1) % Enum.GetValues(typeof(DayOfWeek)).Length));
+        }
+    }
+}
